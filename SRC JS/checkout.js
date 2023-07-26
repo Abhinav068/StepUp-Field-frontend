69efcebc504f6ac4b0e427b2326fc902d97c1e3b
Abhinav068 <abhinav28897@gmail.com>
2023-07-26 21:43:19 +0530

    stepfieldfrontend

---
+++ SRC JS/checkout.js
@@ -0,0 +1,278 @@
+let products = JSON.parse(localStorage.getItem("productsAdd")) || [];
+
+let totalprices = localStorage.getItem("totalprice") || "";
+let productcounts = localStorage.getItem("productcounts") || 0;
+
+
+let loginUserToken = JSON.parse(localStorage.getItem("loginUserToken")) || false;
+console.log("loginUserToken:", loginUserToken);
+let login_name = JSON.parse(localStorage.getItem("login_name")) || [];
+// order summery variables
+let totalprice = document.getElementById("totalprice");
+
+let bagcount = document.getElementById("itemcounts");
+bagcount.textContent = productcounts;
+let estimatedtotal = document.getElementById("estimatedtotal");
+let productcard = document.getElementById("summeryproduct");
+let zipdoller = document.getElementById("zipdoller");
+let URL = `https://fluffy-pantyhose-toad.cyclic.app`;
+// let URL = `http://localhost:8000`;
+// product append
+appendproduct(products);
+function appendproduct(data) {
+  let card = `
+   ${data
+      .map((item) =>
+        getcards(item.image1, item.name, item.size, item.price, item.quantity)
+      )
+      .join("")}
+   `;
+  productcard.innerHTML = card;
+}
+
+function getcards(image, name, size, price, quantity) {
+  /* 
+   let amount=JSON.parse(localStorage.getItem("discount"))
+    // totalp+=price*quantity;
+    totalprice.textContent="₹"+amount;
+    estimatedtotal.textContent="₹"+amount;
+    */
+
+  totalprice.textContent = "₹" + totalprices;
+  estimatedtotal.textContent = "₹" + totalprices;
+
+  let cards = `<div class="products">
+    <div id="image">
+        <img src="${image}" alt="" id="img">
+        <p id="style">Style #38326</p>
+    </div>
+    <div id="info">
+     <p id="title">${name}</p>
+     <p id="shoesize">Size: ${size}.0</p>
+     <p>Width: Medium</p>
+     <p>Color: Brown/Red/Gum</p>
+     <p id="priceofshoe">₹${price}</p>
+     <p class="promo">BOGO 1st pair only</p>
+     <p class="promo">Excluded from Promo</p>
+    </div>
+</div>
+<hr>`;
+  return cards;
+}
+//order summary variable end
+
+// form data catching
+let email = document.getElementById("email");
+let fname = document.getElementById("firstname");
+let lname = document.getElementById("lastname");
+let streetadrs = document.getElementById("streedadds");
+let optionaladrs = document.getElementById("opt");
+let city = document.getElementById("city");
+let state = document.getElementById("state");
+let zipcode = document.getElementById("zipcode");
+let phone = document.getElementById("phonenumber");
+let submitbtn = document.getElementById("submitbtn");
+let cardholdername = document.getElementById("cardholdername");
+let cardnumber = document.getElementById("cardnumber");
+let expirydate = document.getElementById("expirydate");
+let cvv = document.getElementById("cvv");
+// submitbtn.addEventListener("click", razorpay);
+
+submitbtn.addEventListener("click", () => {
+  let obj = {
+    email: email.value,
+    fname: fname.value,
+    lname: lname.value,
+    streetadrs: streetadrs.value,
+    optionaladrs: optionaladrs.value,
+    city: city.value,
+    state: state.value,
+    zipcode: zipcode.value,
+    phone: phone.value,
+  };
+  let carddetails = {
+    holdername: cardholdername.value,
+    cardnumber: cardnumber.value,
+    expirydate: expirydate.value,
+    cvv: cvv.value,
+  };
+  if (loginUserToken != true) {
+    alert("You are not logged in");
+    onclick = "window.location.href='login.html';";
+    return;
+  }
+  if (email.value == "") {
+    window.location.href = "#contact";
+    email.style.border = "2px solid red";
+  } else if (fname.value == "") {
+    window.location.href = "#myaddress";
+    fname.style.border = "2px solid red";
+  } else if (lname.value == "") {
+    window.location.href = "#myaddress";
+    lname.style.border = "2px solid red";
+  } else if (streetadrs.value == "") {
+    window.location.href = "#myaddress";
+    streetadrs.style.border = "2px solid red";
+  } else if (city.value == "") {
+    window.location.href = "#myaddress";
+    city.style.border = "2px solid red";
+  } else if (state.value == "state") {
+    window.location.href = "#myaddress";
+    state.style.border = "2px solid red";
+  } else if (zipcode.value == "") {
+    window.location.href = "#myaddress";
+    zipcode.style.border = "2px solid red";
+  } else if (phone.value == "") {
+    window.location.href = "#myaddress";
+    phone.style.border = "2px solid red";
+  } else if (cardholdername.value == "") {
+    window.location.href = "#paymentcard";
+    cardholdername.style.border = "2px solid red";
+  } else if (cardnumber.value == "") {
+    window.location.href = "#paymentcard";
+    cardnumber.style.border = "2px solid red";
+  } else if (expirydate.value == "") {
+    window.location.href = "#paymentcard";
+    expirydate.style.border = "2px solid red";
+  } else if (cvv.value == "") {
+    window.location.href = "#paymentcard";
+    cvv.style.border = "2px solid red";
+  } else {
+    console.log(obj);
+    console.log(carddetails);
+    razorpay()
+    // storingdatainorderapi();
+  }
+})
+
+// getting data for order page
+function storingdatainorderapi() {
+  let obj = {};
+  obj.firstName = fname.value;
+  obj.lastName = lname.value;
+  obj.Address =
+    streetadrs.value +
+    "," +
+    city.value +
+    "," +
+    state.value +
+    "," +
+    zipcode.value;
+  obj.phoneNo = phone.value;
+  obj.orderValue = totalprices;
+  obj.products = products;
+  console.log("obj", obj);
+  fetch(`${URL}/orders/add`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("orders data");
+      console.log(data);
+      window.location.assign("finalpage.html");
+    });
+}
+
+//closing of form data
+
+// back to cart data link adding here using js because in html link are showing in blue color and I can't do text decoration none there.../
+
+let backtocarttop = document.getElementById("topback");
+backtocarttop.addEventListener("click", () => {
+  window.location.assign("cart.html");
+});
+let backtocartbottom = document.getElementById("backtocart");
+backtocartbottom.addEventListener("click", () => {
+  window.location.assign("cart.html");
+});
+
+// ............Abhinav - Razorpay functions starts here............
+
+
+async function razorpay() {
+  let amount = (+totalprices)*100; //Get from cart value (in paise);
+
+  console.log('payment process started...',totalprices);
+
+  let response = await fetch(`${URL}/payments/create-payment`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ amount })
+  })
+  let order = await response.json();
+  console.log(order);
+  if (response.ok) {
+    makepayment(order);
+  }
+
+}
+
+function makepayment(obj) {
+  let options = {
+    "key": "rzp_test_jz2MPO6H96PjF0",
+    "amount": obj.amount,
+    "currency": "INR",
+    "name": "Dummy Academy",
+    "description": "Pay & Checkout this Course, Upgrade your DSA Skill",
+    "image": "https://media.geeksforgeeks.org/wp-content/uploads/20210806114908/dummy-200x200.png",
+    "order_id": obj.id,
+    "handler": function (response) {
+      alert("This step of Payment Succeeded");
+      verifypayment(response);
+    },
+    "prefill": {
+      //Here we are prefilling random contact
+      "contact": "9876543210",
+      //name and email id, so while checkout
+      "name": "abhinav",
+      "email": "abhinav@gmail.com",
+    },
+    "notes": {
+      "description": "Best Course for SDE placements",
+      "language": "Available in 4 major Languages JAVA,C/ C++, Python, Javascript",
+      "access": "This course have Lifetime Access"
+    },
+    "theme": {
+      "color": "#2300a3"
+    }
+  };
+
+  let razorpayObject = new Razorpay(options);
+  razorpayObject.on('payment.failed', function (response) {
+    alert("This step of Payment Failed");
+  });
+  razorpayObject.open();
+}
+
+async function verifypayment(obj){
+  console.log('verification initiated...');
+  let response = await fetch(`${URL}/payments/verifyOrder`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-razorpay-signature': obj.razorpay_signature
+    },
+    body: JSON.stringify({ order_id:obj.razorpay_order_id, payment_id:obj.razorpay_payment_id })
+  })
+  let order = await response.json();
+  console.log(order);
+  if (response.ok) {
+    if(order.success) {
+      alert('Payment Verified')
+      storingdatainorderapi();
+    }
+    else{
+      alert('Payment couldn\'t verified');      
+    }
+  }
+  else{
+    alert (order);
+  }
+}
+
---
+++ SRC JS/product.js
@@ -0,0 +1,394 @@
+let productdata = JSON.parse(localStorage.getItem("product")) || {};
+let cartData = JSON.parse(localStorage.getItem("productsAdd")) || [];
+let loginUserToken = localStorage.getItem("loginUserToken") || false; 
+console.log("loginUserToken:", loginUserToken);
+let container = document.getElementById("product-container");
+let login_name = JSON.parse(localStorage.getItem("login_name")) || [];
+console.log(productdata);
+
+//cart item count start
+let productcounts = localStorage.getItem("productcounts") || 0;
+// let itemcounts = document.getElementById("itemcounts");
+// itemcounts.textContent = productcounts;
+//cart item count code end
+
+container.innerHTML = ` <div id="roll">
+<p>Shoes / ${productdata.gender} / Sneakers and Athletic Shoes / Lifestyle and Fashion Sneakers</p>
+</div>
+<div id="container1">
+<div id="cont">
+<img src="${productdata.image1}" alt="">
+<img src="${productdata.image2}" class="img-top" alt="">
+</div>
+<div id="box">
+<h1>${productdata.name}</h1>
+<h4 id="gender">${productdata.gender}</h4>
+<h2 id="price">₹${productdata.price}</h2>
+<h2 id="brand">${productdata.brand}</h2>
+<h3 id="size">Size:</h3>
+
+<div class="btn">
+<button class="one active">5</button>
+<button class="one">6</button>
+<button class="one">7</button>
+<button class="one">8</button>
+<button class="one">9</button>
+<button class="one">10</button>
+<button class="one">11</button>
+<button class="one">12</button>
+</div>
+
+<h3>color:black/white</h3>
+<h4>Width:</h4>
+<button>Medium</button>
+
+
+<h3>Size Chart</h3>
+ <input type="radio">
+ <span style="color:grey;">Store Pickup - FREE</span>
+ <h5>Find a Store for pickup options</h5><br>
+<input type="radio">
+<span style="color:grey;">Ship to Address</span>
+<h5>Receive in 4-7 business days with standard</h5>
+
+<button id="btn">ADD TO CART</button>
+
+<h5><span style="color:rgb(235, 19, 55);">Enjoy 10% OFF Today*</span> on your first purchase with the <br>
+ FAMOUSLY YOU REWARDS® Credit Card at time of <br>
+ account opening.Learn More</h5>
+ </div>
+ </div>
+<div id="cool">
+    <h3>ITEM DETAILS</h3>
+    <p>Style #40087</p>
+    <p> Stand tall in cool style with the Women's Madden Girl Vaultt Wedge sandal.</p>
+    <ul type="circle">
+        <li>EVA upper in a wedge sandal style with an open toe</li> <br>
+        <li>Buckled ankle strap closure</li> <br>
+        <li> Multi strap detail</li> <br>
+        <li>Smooth lining with a padded footbed</li> <br>
+        <li>Durable rubber outsole with a 3.5" wedge heel</li>
+    </ul>
+
+</div>`;
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  // addTocart(el);
+  let clickcount = 0;
+  clickcount++;
+  console.log(loginUserToken);
+  if (!loginUserToken ) {
+    alert("Please Log in First");
+  } else {
+    if (duplicate(productdata) == false) {
+      //products count
+      if (productcounts == 0) {
+        itemcounts.textContent = clickcount;
+        localStorage.setItem("productcounts", clickcount);
+      } else {
+        productcounts = localStorage.getItem("productcounts") || 0;
+        productcounts = Number(productcounts) + clickcount;
+        itemcounts.textContent = productcounts;
+        localStorage.setItem("productcounts", productcounts);
+      }
+      //products count end
+
+      cartData.push({ ...productdata, quantity: 1 });
+
+      console.log(cartData);
+      console.log("cartData");
+      console.log(cartData.quantity);
+      localStorage.setItem("productsAdd", JSON.stringify(cartData));
+      //   cartadding.innerText = "Product Added To Cart";
+      alert("Product Successfully added to Cart");
+      // console.log("login", JSON_flag_signin);
+    } else if (duplicate(productdata) == true) {
+      //product count
+      productcounts = localStorage.getItem("productcounts") || 0;
+      productcounts = Number(productcounts) + clickcount;
+      console.log(productcounts);
+      itemcounts.textContent = productcounts;
+      localStorage.setItem("productcounts", productcounts);
+      //product count end
+
+      cartData.forEach((element) => {
+        if (element.id == productdata.id) {
+          element.quantity++;
+
+          console.log(element);
+          localStorage.setItem("productsAdd", JSON.stringify(cartData));
+        }
+      });
+      alert("Product Successfully added to Cart");
+    }
+
+    //window.location.href = "cart.html";
+  }
+});
+function duplicate(product) {
+  for (let i = 0; i < cartData.length; i++) {
+    if (cartData[i].id == product.id) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+var header = document.getElementsByClassName("btn");
+var btns = document.getElementsByClassName("one");
+console.log(btns);
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+////login/signout option on navbar
+
+/////signout/log in button
+
+//let loginButton = document.getElementById("loginButton");
+//let signout = document.getElementById("signoutButton");
+// let signout = document.getElementById("loginlogout");
+// if (loginUserToken == true) {
+//   console.log(login_name);
+//   console.log("done");
+//   loginButton.innerText = "Hi,  " + login_name;
+// }
+
+// if (loginUserToken == true) {
+//   console.log("yes");
+//   signout.innerText = "Hi,  " + login_name + "                   Sign Out";
+//   signout.style.fontSize = "13px";
+//   signout.style.fontWeight = "bolder";
+//   signout.style.cursor = "pointer";
+//   signout.style.marginTop = "-28px";
+//   signout.style.marginLeft = "280px";
+  // signout.addEventListener("click", () => {
+  //   loginUserToken = false;
+  //   login_name = "";
+  // });
+// }
+
+/*let loginButton = document.getElementById("loginButton");
+let signout = document.getElementById("signoutButton");
+loginandlogoutnames();
+function loginandlogoutnames(){
+  if (loginUserToken == true) {
+    //console.log(login_name);
+    loginButton.innerText = "Hi,  " + login_name;
+  }
+  
+  if (loginUserToken == true) {
+    signout.innerText = "Sign Out";
+    loginButton.style.fontSize = "14px";
+    signout.addEventListener("click", () => {
+      console.log("clicked on signout")
+      loginUserToken = false;
+      login_name = "";
+    });
+  }
+}
+let loginlogout=document.getElementById("loginlogout")*/
+
+// signout.addEventListener("click", () => {
+//   loginUserToken = false;
+//   login_name = "";
+//   cartData = [];
+//   localStorage.setItem("cart", JSON.stringify(cartData));
+//   localStorage.setItem("loginUser", JSON.stringify(loginUserToken));
+//   localStorage.setItem("login_name", JSON.stringify(login_name));
+//   localStorage.setItem("productsAdd", JSON.stringify(cartData));
+//   window.location.href = "index.html";
+// });
+
+
+// Abhinav- Review part
+let userid = localStorage.getItem('userID') || null;
+let usertoken = localStorage.getItem('token') || null;
+
+function checklogin() {
+  if (!loginUserToken) {
+    window.location.href = './login.html';
+    return;
+  }
+}
+
+let form = document.querySelector('.container form')
+// const URL = 'http://localhost:8000'
+const URL = 'https://fluffy-pantyhose-toad.cyclic.app'
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let title = form.rvtitle.value;
+  let description = form.rvdes.value;
+  let rating = +form.rating.value;
+  let payload = { title, rating, description }
+  console.log(payload);
+  sendReview(payload);
+})
+
+async function sendReview(payload) {
+try {
+  console.log(payload);
+  let response = await fetch(`${URL}/products/comment/${productdata.id}`, {
+
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': usertoken
+    },
+    body: JSON.stringify(payload)
+  })
+  console.log(response);
+  if (response.ok) {
+    history.back();
+    getallcomments();
+  }
+  let result = await response.json();
+  console.log(result);
+  
+} catch (error) {
+  console.log(error);
+}
+
+}
+
+getallcomments();
+async function getallcomments() {
+  let response = await fetch(`${URL}/products/comments/${productdata.id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': usertoken
+    }
+  })
+  let result = await response.json();
+  console.log(result.comments);
+  appendReviews(result.comments)
+}
+
+function appendReviews(data) {
+  console.log('append', data);
+  let parent = document.getElementById('userreviews');
+  parent.innerHTML = '';
+  parent.innerHTML = data.map(el =>
+    `<div class="reviewcard">
+    <p>${el.name}</p> 
+    ${(() => {
+      let stars = '';
+      for (let i = 0; i < 5; i++) {
+        if (i < el.rating) {
+          stars += `<span class="fa fa-star checked"></span>`;
+        }
+        else {
+          stars += `<span class="fa fa-star"></span>`;
+        }
+      }
+      return stars;
+    })()
+    }
+
+    <span>${el.title}</span>
+    <p> ${el.description} </p> 
+    <span>${el.date.split('').slice(0, 10).join('')}</span> <br>
+    ${userid == el.userID ? `<button data-id='${el._id}'>Delete</button>` : ''}   
+    </div>
+    `
+  ).join('')
+  // ${userid==el.userID?`<button onclick="deleteComment('${userid}','${el._id}')">Delete</button>`:''}   
+
+  let delbtn = document.querySelector('.reviewcard>button');
+  if(delbtn){
+  delbtn.addEventListener('click', async (e) => {
+    console.log(e.target.dataset.id);
+    let response = await fetch(`${URL}/products/comments/${e.target.dataset.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': usertoken
+      }
+    })
+    let result = await response.json();
+    if (response.ok) {
+      alert(result.msg)
+      e.target.parentNode.remove();
+      getallstars()
+    }
+    else {
+      console.log(result.msg);
+    }
+
+  })}
+  getallstars();
+
+}
+
+async function getallstars() {
+  document.querySelector('.imgload').style.display='none'; 
+  let response = await fetch(`${URL}/products/stars/${productdata.id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': usertoken
+    }
+  })
+  let result = await response.json();
+  console.log(result);
+  result=result.ans;
+  let [total_users, total_ratings] = [0, 0];
+
+  for (let i in result) {
+    total_users += +result[i];
+    total_ratings += (+i * (+result[i]));
+  }
+  document.getElementById(`ratingchart`).innerHTML = ``;
+  for (let i = 5; i > 0; i--) {
+    document.getElementById(`ratingchart`).innerHTML += `
+    <div>
+    <span>${i} stars</span> &ensp;
+    <meter id="star${i}" value="${result[i] / total_users}"></meter> <span>${result[i]}</span>
+    </div>`
+  }
+
+  if(!(total_ratings / total_users)) {
+    document.getElementById('ratings').innerHTML = `<h3>No Ratings Available</h3>`;
+    return
+  }
+  document.getElementById('ratings').innerHTML = `
+  <h3>Overall Ratings</h3>
+  <p>
+      <span>${(total_ratings / total_users).toFixed(1)}</span> <span>
+      ${(() => {
+      let avg = total_ratings / total_users;
+      let stars = '';
+      let count = 5;
+      console.log(Math.floor(avg));
+      for (let i = 0; i < Math.floor(avg); i++) {
+        stars += '<span class="fa fa-star checked" style="font-size:25px"></span>';
+        count--;
+      }
+      if (avg % 1){
+        stars += '<i class="fa fa-star-half-full checked" style="font-size:25px"></i>';
+        count--;
+        
+      }
+      if (count) {
+        for (let i = 0; i < count; i++) {
+          stars += '<span class="fa fa-star" style="font-size:25px"></span>';
+        }
+
+      }
+      return stars;
+    })()
+    }
+      </span>
+  </p>
+  <p>${(total_users)} Reviews</p>
+  `
+}
+
+// getallstars();
